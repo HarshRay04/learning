256f6e5af47b4a68e672547a31a21ab346d2dffb
Harsh Ray <harshray4u4@gmail.com>
2025-09-05 22:26:50 +0530

    some more about functions.

--- functions.js
+++ functions.js
@@ -17,13 +17,26 @@ printName("Gaurav");
 -> the envirnment is called execution context.
 -> when intially the programme runs the execution context is global.
 -> which is termed as GEC = global execution context.
-->everytime a function is called there is crreated a local
     execution context and pushed into call stack.
 -> the execution context has two main components:
  :-> the first phase where there is memory allocation to the variable and all
     it is also called variable enivronment;
- :-> the second phase is basically the code phase where the code is executed 
+ :-> the second phase is basically the code phase where the code is executed K
     one by one or we can say line by line as js is
     synchronus single threaded language,that is that all the code
     is executed in the same thread and one by one;
 */
+
+
+/* 
+-> let and const are block scoped.
+-> var is function scope.
+*/
+
+// functions are called first class citizens.
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add);
+console.log(2, 3);
